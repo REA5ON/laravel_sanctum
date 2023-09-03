c8fac75366ae618abe8ee7525a95d76717b0b5e3
REA5ON <valten007@gmail.com>
2023-09-03 22:00:40 +0200

    make BaseController for Auth

---
+++ app/Http/Controllers/Api/Auth/BaseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+class BaseController extends Controller
+{
+    public function sendResponse($result, $message): JsonResponse
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMessages = [], $code = 404): JsonResponse
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
